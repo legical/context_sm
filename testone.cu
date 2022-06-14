d94e24b54445092041048aae848d5c0631ca29f5
cxg_Bric <chuxuange@gmail.com>
2022-06-14 22:22:52 +0800

    one

---
+++ testone.cu
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <cuda.h>
+#include "util.cu"
+#include <iostream>
+#include <utility>
+#include <thread>
+using namespace std;
+
+int main(void) {
+
+    return 0;
+}
