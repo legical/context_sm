5d21f11ea7ed2325719400f56809c87ed44d5a59
Bric-R <chuxuange@gmail.com>
2022-06-29 23:39:56 +0800

    sleep

--- duration.cu
+++ duration.cu
@@ -7,27 +7,31 @@
 #include <sys/time.h>
 #define USECPSEC 1000000ULL
 
-unsigned long long dtime_usec(unsigned long long start){
-  timeval tv;
-  gettimeofday(&tv, 0);
-  return ((tv.tv_sec*USECPSEC)+tv.tv_usec)-start;
+unsigned long long dtime_usec(unsigned long long start) {
+    timeval tv;
+    gettimeofday(&tv, 0);
+    return ((tv.tv_sec * USECPSEC) + tv.tv_usec) - start;
 }
 
-#define APPRX_CLKS_PER_SEC 1000000000ULL
-__global__ void delay_kernel(unsigned seconds){
-  unsigned long long dt = clock64();
-  while (clock64() < (dt + (seconds*APPRX_CLKS_PER_SEC)));
+// #define APPRX_CLKS_PER_SEC 1000000000ULL
+__global__ void delay_kernel(unsigned seconds) {
+    unsigned long long dt = 0;
+    // while (clock64() < (dt + (seconds * APPRX_CLKS_PER_SEC)))
+    //     ;
+    while (dt++ < seconds) {
+        printf("");
+    }
 }
 
-int main(int argc, char *argv[]){
-  unsigned delay_t = 10; // seconds, approximately
-  // unsigned delay_t_r;
-  // if (argc > 1) delay_t_r = atoi(argv[1]);
-  // if ((delay_t_r > 0) && (delay_t_r < MAX_DELAY)) delay_t = delay_t_r;
-  unsigned long long difft = dtime_usec(0);
-  delay_kernel<<<1,1>>>(delay_t);
-  cudaDeviceSynchronize();
-  difft = dtime_usec(difft);
-  printf("kernel duration: %fs\n", difft/(float)USECPSEC);
-  return 0;
+int main(int argc, char* argv[]) {
+    unsigned delay_t = 2 << 8; // seconds, approximately
+    // unsigned delay_t_r;
+    // if (argc > 1) delay_t_r = atoi(argv[1]);
+    // if ((delay_t_r > 0) && (delay_t_r < MAX_DELAY)) delay_t = delay_t_r;
+    unsigned long long difft = dtime_usec(0);
+    delay_kernel<<<1, 1>>>(delay_t);
+    cudaDeviceSynchronize();
+    difft = dtime_usec(difft);
+    printf("kernel duration: %fs\n", difft / (float)USECPSEC);
+    return 0;
 }
