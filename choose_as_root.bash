9eaf83b5e0a489485208c4f369f844bfb55f67fb
bric-U <chuxuange@gmail.com>
2022-06-30 22:02:40 +0800

    f

--- choose_as_root.bash
+++ choose_as_root.bash
@@ -3,7 +3,7 @@
 #compute_mode  = 0/Default, 1/Exclusive_Thread, 2/Prohibited, 3/Exclusive_Process
 #target_gpu_id = Id number found by running the following command: nvidia-smi -q
 #for my device it would be: nvidia-smi -c 3 -i 00000000:0B:00.0
-echo -e "***** Note: chmod +x this script firstly and used with \033[31mroot\033[0m privilege ! *****"
+echo -e "***** Note: Firstly chmod +x this script and used as \033[31mroot\033[0m privilege ! *****"
 echo -e "***** \033[32mStart\033[0m MPS in \033[32mY\033[0m  or  \033[34mStop\033[0m MPS in \033[34mN\033[0m *****"
 read -p "***** Please enter your choice : " ANSWER
 case "$ANSWER" in
