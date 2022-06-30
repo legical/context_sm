f3d0f9cdbd244645376c56ccb86030d9eebc165e
Bric-R <chuxuange@gmail.com>
2022-06-30 21:32:24 +0800

    echo

--- choose_as_root.bash
+++ choose_as_root.bash
@@ -3,25 +3,25 @@
 #compute_mode  = 0/Default, 1/Exclusive_Thread, 2/Prohibited, 3/Exclusive_Process
 #target_gpu_id = Id number found by running the following command: nvidia-smi -q
 #for my device it would be: nvidia-smi -c 3 -i 00000000:0B:00.0
-echo -e "***** Note: This script must be performed with \033[31mroot\033[0m privilege ! *****"
+echo -e "***** Note: chmod +x this script firstly and performed with \033[31mroot\033[0m privilege ! *****"
 # echo "Note: This script must be performed with root privilege !"
-echo -e "***** \033[32mStart\033[0m MPS in \033[32mY\033[0m  or  \033[31mStop\033[0m MPS in \033[31mN\033[0m *****"
+echo -e "***** \033[32mStart\033[0m MPS in \033[32mY\033[0m  or  \033[34mStop\033[0m MPS in \033[34mN\033[0m *****"
 read -p "***** Please enter your choice : " ANSWER
 case "$ANSWER" in
   [yY] | [yY][eE][sS])
-    echo "***** Your choice is : Start MPS *****"
+    echo "***** Your choice is : \033[32mStart MPS\033[0m *****"
     export CUDA_VISIBLE_DEVICES="0"
     nvidia-smi -i 0 -c EXCLUSIVE_PROCESS
     nvidia-cuda-mps-control -d
-    echo "Start over, exit!"
+    echo "***** \033[32mStart over, exit!\033[0m *****"
     ;;
   [nN] | [nN][oO])
-    echo "Your choice is : Stop MPS"
+    echo "***** Your choice is : \033[34mStop MPS\033[0m *****"
     echo quit | nvidia-cuda-mps-control
     nvidia-smi -i 0 -c DEFAULT
-    echo "Stop over, exit!"
+    echo "***** \033[34mStop over, exit!\033[0m *****"
     ;;
   *)
-    echo "Invalid Choice : Exit!"
+    echo "***** \033[31mInvalid Choice : Exit!\033[0m *****"
     ;;
 esac
