e6a04363afa89023dedc72e5c40edbe30405d343
Bric-R <chuxuange@gmail.com>
2022-06-30 21:23:57 +0800

    echo

--- choose_as_root.bash
+++ choose_as_root.bash
@@ -3,12 +3,13 @@
 #compute_mode  = 0/Default, 1/Exclusive_Thread, 2/Prohibited, 3/Exclusive_Process
 #target_gpu_id = Id number found by running the following command: nvidia-smi -q
 #for my device it would be: nvidia-smi -c 3 -i 00000000:0B:00.0
-echo "Note: This script must be performed with root privilege !"
-echo "Start MPS in Y  or  Stop MPS in N"
-read -p "Please enter your choice : " ANSWER
+echo -e "***** Note: This script must be performed with \033[31mroot\033[0m privilege ! *****"
+# echo "Note: This script must be performed with root privilege !"
+echo -e "***** \033[32mStart\033[0m MPS in \033[32mY\033[0m  or  \033[31mStop\033[0m MPS in \033[31mN\033[0m *****"
+read -p "***** Please enter your choice : " ANSWER
 case "$ANSWER" in
   [yY] | [yY][eE][sS])
-    echo "Your choice is : Start MPS"
+    echo "***** Your choice is : Start MPS *****"
     export CUDA_VISIBLE_DEVICES="0"
     nvidia-smi -i 0 -c EXCLUSIVE_PROCESS
     nvidia-cuda-mps-control -d
