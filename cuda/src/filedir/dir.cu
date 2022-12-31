07dac84e42d971105cada81e4a1e6404901b4e0e
Bric-R <chuxuange@gmail.com>
2022-12-31 23:07:52 +0800

    getdir

--- cuda/src/filedir/dir.cu
+++ cuda/src/filedir/dir.cu
@@ -6,15 +6,15 @@
 #include <cuda.h>
 int main(int argc, char *argv[])
 {
-    printf("1. _pgmptr: %s\n", _pgmptr);
+    // printf("1. _pgmptr: %s\n", _pgmptr);
 
     char path[64];
     getcwd(path, sizeof(path));
-    printf("2. getcwd: %s\n", path);
+    printf("1. getcwd: %s\n", path);
 
-    printf("3. __FILE__: %s\n", __FILE__);
+    printf("2. __FILE__: %s\n", __FILE__);
 
-    printf("4. argv[0]: %s\n", argv[0]);
+    printf("3. argv[0]: %s\n", argv[0]);
     
     return 0;
 }
