5e222fe90f3af9fa31fd4616a30ba1ec35a2e265
Bric-R <chuxuange@gmail.com>
2022-12-31 23:24:33 +0800

    path

--- cuda/src/filedir/dir.cu
+++ cuda/src/filedir/dir.cu
@@ -14,8 +14,8 @@ int main(int argc, char *argv[])
     printf("1. getcwd: %s\n", path);
 
     printf("2. __FILE__: %s\n", __FILE__);
-    printf("2.2 dir__FILE__: %s\n", dirname(__FILE__));
-    printf("2.3 base__FILE__: %s\n", basename(__FILE__));
+    printf("2.2 dir__FILE__: %s\n", dirname(path));
+    printf("2.3 base__FILE__: %s\n", basename(path));
     
 
     printf("3. argv[0]: %s\n", argv[0]);
