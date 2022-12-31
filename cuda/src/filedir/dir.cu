6e33c8763cffc275b699d44747db6f36831b2981
Bric-R <chuxuange@gmail.com>
2022-12-31 23:02:15 +0800

    cmake

--- cuda/src/filedir/dir.cu
+++ cuda/src/filedir/dir.cu
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <unistd.h>
 #include <cuda.h>
-#include <cuda_runtime.h>
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
-  printf("1. _pgmptr: %s\n",_pgmptr);
+    printf("1. _pgmptr: %s\n", _pgmptr);
 
-  char path[64]; //PATH_MAX is defined in limits.h
-  getcwd(path,sizeof(path));
-  printf("2. getcwd: %s\n",path);
+    char path[64];
+    getcwd(path, sizeof(path));
+    printf("2. getcwd: %s\n", path);
 
-  printf("3. __FILE__: %s\n",__FILE__);
+    printf("3. __FILE__: %s\n", __FILE__);
 
-  printf("4. argv[0]: %s\n",argv[0]);
-//   system("pause");
-  return 0;
+    printf("4. argv[0]: %s\n", argv[0]);
+    
+    return 0;
 }
