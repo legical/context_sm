d1a1b2596a7dc3880b56485ca79c084ccbf5135a
Bric-R <chuxuange@gmail.com>
2022-12-31 22:45:57 +0800

    test get file dir

---
+++ cuda/src/filedir/dir.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc,char *argv[])
+{
+  printf("1. _pgmptr: %s\n",_pgmptr);
+
+  char path[64]; //PATH_MAX is defined in limits.h
+  getcwd(path,sizeof(path));
+  printf("2. getcwd: %s\n",path);
+
+  printf("3. __FILE__: %s\n",__FILE__);
+
+  printf("4. argv[0]: %s\n",argv[0]);
+//   system("pause");
+  return 0;
+}
