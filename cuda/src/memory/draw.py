2b342ef16a1afad26c43bed7126f31344a33a875
Bric-R <chuxuange@gmail.com>
2023-01-01 01:01:33 +0800

    minax[0]

--- cuda/src/memory/draw.py
+++ cuda/src/memory/draw.py
@@ -17,7 +17,7 @@ def readname():
     return csv
 
 
-def get_data(filename, IDlist, EXEClist, min, max):
+def get_data(filename, IDlist, EXEClist, minax):
     # '''get the highs and lows from a data file'''
     with open(filename) as f:
         reader = csv.reader(f)
@@ -27,10 +27,10 @@ def get_data(filename, IDlist, EXEClist, min, max):
                 IDlist.append(float(row[0]))
                 time = float(row[1])
                 EXEClist.append(time)
-                if min > time:
-                    min = time
-                if max < time:
-                    max = time
+                if minax[0] > time:
+                    minax[0] = time
+                if minax[1] < time:
+                    minax[1] = time
             except ValueError:
                 print(row[0], 'reading data error!\n')
 
@@ -45,15 +45,14 @@ for file in csvlist:
 
     # 获取ID
     IDlist, EXEClist = [], []
-    min = 99999999
-    max = 0
+    minax = [99999999.0,0.0]
     get_data(filename, IDlist, EXEClist, min, max)
 
     plt.plot(IDlist, EXEClist, "g", marker='D', markersize=5, label="执行时间")
 
     # y轴刻度值
-    min_y_lable = np.floor(min)-10
-    max_y_lable = np.ceil(max)+2
+    min_y_lable = np.floor(minax[0])-10
+    max_y_lable = np.ceil(minax[1])+2
     plt.yticks(np.arange(min_y_lable, max_y_lable, 0.2))
 
     # plt.title('执行时间折线图')  # 折线图标题
@@ -62,7 +61,7 @@ for file in csvlist:
     # 设置 y 轴显示网格线
     plt.grid(axis='y')
     print('min/max/min_y_lable/max_y_lable : ',
-          min, max, min_y_lable, max_y_lable, '\n')
+          minax[0], minax[1], min_y_lable, max_y_lable, '\n')
     filename = filename.replace("./output/", "./output/pic/", 1)
     pic_name = filename.replace("csv", "jpg", 1)
     # 如果图片文件已存在，则删除
