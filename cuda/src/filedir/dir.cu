0e8d9c81e4de5bef07ed28a6071468bcc21b8d98
Bric-R <chuxuange@gmail.com>
2022-12-31 23:22:20 +0800

    basename

--- cuda/src/filedir/dir.cu
+++ cuda/src/filedir/dir.cu
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <cuda.h>
+#include <libgen.h>
 int main(int argc, char *argv[])
 {
     // printf("1. _pgmptr: %s\n", _pgmptr);
@@ -13,6 +14,9 @@ int main(int argc, char *argv[])
     printf("1. getcwd: %s\n", path);
 
     printf("2. __FILE__: %s\n", __FILE__);
+    printf("2.2 dir__FILE__: %s\n", dirname(__FILE__));
+    printf("2.3 base__FILE__: %s\n", basename(__FILE__));
+    
 
     printf("3. argv[0]: %s\n", argv[0]);
     
