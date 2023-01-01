cf40aac77e73c313fa3f50701c4af6f34f275931
Bric-R <chuxuange@gmail.com>
2023-01-01 23:38:57 +0800

    change lib dir；write run.sh

--- cuda/src/memory/draw.py
+++ cuda/src/memory/draw.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from cProfile import label
 import os
 import matplotlib.pyplot as plt
--- cuda/src/memory/run.sh
+++ cuda/src/memory/run.sh
@@ -1,14 +1,82 @@
 #!/bin/bash
-script_dir=$(cd $(dirname $0);pwd)
-dir=$(dirname $script_dir)
-
+script_dir=$(
+    cd $(dirname $0)
+    pwd
+)
 # /home/bric/Workspace/context_sm/cuda/src/memory
-echo "$script_dir"
 
+dir=$(dirname $script_dir)
 # /home/bric/Workspace/context_sm/cuda/src
-echo "$dir"
+
+# 检查csv输出目录是否存在
+if [ -d $script_dir/output ]; then
+    chmod 754 $script_dir/output
+    echo ".csv file output directory already exists."
+else
+    mkdir -m 754 $script_dir/output
+    echo ".csv file output directory successfully created."
+fi
+# 检查pic输出目录是否存在
+if [ -d $script_dir/output/pic ]; then
+    chmod 754 $script_dir/output/pic
+    echo ".jpg file output directory already exists."
+else
+    mkdir -m 754 $script_dir/output/pic
+    echo ".jpg file output directory successfully created."
+fi
+
+# 获取参数
+EXEC_TIMES = 1000
+ARR_SIZE = 1073741824
+if [ $# ] >0; then
+    EXEC_TIMES = $1
+fi
+if [ $# ] >1; then
+    ARR_SIZE = $2
+fi
+echo "You have entered $# parameter."
+echo "EXEC_TIMES: $EXEC_TIMES       ARR_SIZE: $ARR_SIZE"
 
 # /home/bric/Workspace/context_sm/cuda
-echo $(dirname $dir)
+cd $(dirname $dir)
+# 检查 cmake build 目录是否存在
+if [ -d build ]; then
+    chmod 754 build
+    echo "Cmake build directory already exists."
+else
+    mkdir -m 754 build
+    echo "Cmake build directory successfully created."
+fi
 
-echo $#
+# 进入 build, 生成项目
+cd build
+echo "Start compiling the project......"
+cmake .. && make
+
+# 执行项目
+echo "Start running the project......"
+./cu_ran $EXEC_TIMES $ARR_SIZE
+
+# 根据数据画图
+echo "Draw line charts according to the output data......"
+cd $script_dir
+# 判断是否安装了 matplotlib 模块
+function python_model_check() {
+    if python -c "import $1" >/dev/null 2>&1; then
+        return 1
+    else
+        return 0
+    fi
+}
+python_model_check matplotlib
+if [ $? == 1 ]; then
+    echo "Matplotlib has been installed."
+else
+    echo "Installing matplotlib."
+    python3 -m pip install matplotli
+fi
+
+python3 draw.py
+echo "$script_dir/output to see execution time data."
+echo "$script_dir/output/pic to see line charts."
+echo "ALL done."
