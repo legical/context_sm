08d3335d7b97c60ae21104035c5dbe2fc1eaddcf
bric-U <chuxuange@gmail.com>
2023-01-01 19:48:12 +0800

    shell-try file dir

--- cuda/src/memory/run.sh
+++ cuda/src/memory/run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 script_dir=$(cd $(dirname $0);pwd)
 dir=$(dirname $script_dir)
+
+# /home/bric/Workspace/context_sm/cuda/src/memory
 echo "$script_dir"
+
+# /home/bric/Workspace/context_sm/cuda/src
 echo "$dir"
+
+# /home/bric/Workspace/context_sm/cuda
+echo $(dirname $dir)
+
+echo $#
