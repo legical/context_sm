8a44e0475533a32ff6eaf7fb9dc98722de39e180
Bric-R <chuxuange@gmail.com>
2023-01-03 00:07:19 +0800

    del build instead of cmake clean

--- cuda/src/memory/run.sh
+++ cuda/src/memory/run.sh
@@ -71,16 +71,14 @@ fi
 cd $(dirname $dir)
 # 检查 cmake build 目录是否存在
 if [ -d build ]; then
-    chmod 754 build
-    echo "Cmake build directory already exists."
-else
-    mkdir -m 754 build
-    echo -e "\033[34mCmake build directory successfully created.\033[0m"
+    rm -r build
+    # echo "Cmake build directory already exists."
 fi
+mkdir -m 754 build
+echo -e "\033[34mCmake build directory successfully created.\033[0m"
 
 # 进入 build, 生成项目
 cd build
-rm -r ./*
 echo -e "\n\033[34mStart compiling the project......\033[0m"
 cmake .. && make
 
