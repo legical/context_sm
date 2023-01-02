20d94737a328aa0d884ead4bc76dbca5425af245
bric-U <chuxuange@gmail.com>
2023-01-02 23:58:50 +0800

    random run data

---
+++ cuda/build/CMakeFiles/cu_ran.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cu_ran.dir/src/memory/random.cu.o"
+  "CMakeFiles/cu_ran.dir/src/memory/random.cu.o.d"
+  "cu_ran"
+  "cu_ran.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/cu_ran.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cuda/build/CMakeFiles/cu_ran.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cu_ran.
+# This may be replaced when dependencies are built.
