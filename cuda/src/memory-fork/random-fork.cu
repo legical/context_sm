58ceab0c3f88c73546bb8cd6ac3ff76428447cd8
Bric-R <chuxuange@gmail.com>
2023-01-06 01:25:04 +0800

    filename

--- cuda/src/memory-fork/random-fork.cu
+++ cuda/src/memory-fork/random-fork.cu
@@ -15,16 +15,15 @@ int getopt(int argc, char *argv[], int &Index, int &EXEC_TIMES, int &ARR_SIZE, c
             // }
         }
     }
-
+    char path[96];
+    getcwd(path, sizeof(path));
     if (argc > 3)
     {
         sprintf(filename, "%s/src/memory-fork/output/Ran%s.csv",
-                dirname(path), &argv[4]);
+                dirname(path), &argv[3]);
     }
     else
     {
-        char path[96];
-        getcwd(path, sizeof(path));
         sprintf(filename, "%s/src/memory-fork/output/Random-%d.csv",
                 dirname(path), EXEC_TIMES);
     }
@@ -33,10 +32,13 @@ int getopt(int argc, char *argv[], int &Index, int &EXEC_TIMES, int &ARR_SIZE, c
 
 void name(char buf[], char *filename)
 {
-    char path[96];
-    getcwd(path, sizeof(path));
-    sprintf(filename, "%s/src/memory-fork/output/Ran%s.csv",
-            dirname(path), buf);
+    int num = 0;
+    for (int i = 0; i < strlen(buf); i++)
+    {
+        // 通过减去'0'可以将字符转换为int类型的数值
+        num = num * 10 + buf[i] - '0';
+    }
+    return num;
 }
 
 __global__ void read_random_arr(int *arr_gpu, const int ARR_SIZE)
