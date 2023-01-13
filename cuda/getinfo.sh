a94e5da7c2f3a847872c35f99415b0807c31b58a
bric-U <chuxuange@gmail.com>
2023-01-14 01:51:24 +0800

    getinfo

--- cuda/getinfo.sh
+++ cuda/getinfo.sh
@@ -13,8 +13,25 @@ fi
 mkdir -m 754 build
 echo -e "\033[34mCmake build directory successfully created.\033[0m"
 
+if [ -d info ]; then
+    chmod 754 info
+    # echo "Cmake build directory already exists."
+fi
+mkdir -m 754 info
+
 # 进入 build, 生成项目
 cd build
 echo -e "\n\033[34mStart compiling the project......\033[0m"
 cmake .. && make
 
+cfg="cfg.png"
+bbcfg="bbcfg.png"
+line="line.txt"
+
+cuobjdump cu_ran_test -xelf cu_ran_test.1.sm_86.cubin
+nvdisasm -cfg cu_ran_test.1.sm_86.cubin | dot -o$cfg -Tpng
+nvdisasm -bbcfg cu_ran_test.1.sm_86.cubin | dot -o$bbcfg -Tpng
+nvdisasm -gi cu_ran_test.1.sm_86.cubin > $line 2>&1
+
+mv $cfg $bbcfg $line $script_dir/info
+cd $script_dir && rm -rf build
