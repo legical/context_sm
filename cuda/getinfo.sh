03c51f090b80b9fa664f17a4f8de658475bd7f7d
Bric-R <chuxuange@gmail.com>
2023-01-15 03:02:14 +0800

    输出每个项目的汇编代码

--- cuda/getinfo.sh
+++ cuda/getinfo.sh
@@ -24,21 +24,30 @@ cd build
 echo -e "\n\033[34mStart compiling the project......\033[0m"
 cmake .. && make
 
-cfg="add-cfg-cv.png"
-bbcfg="add-bbcfg-cv.png"
-line="add-line-cv.txt"
-# proname="cu_ran_test"
-proname="vector_add"
+# 输出每个项目的汇编代码
+pro_name=('cu_ran_test' 'cu_ran_fork' 'vector_add')
+mkdir $script_dir/info/sass-ptx
+for name in ${pro_name[@]}; do
+    cuobjdump ${name} -sass -ptx >${name}.txt 2>&1
+    mv -f ${name}.txt $script_dir/info/sass-ptx
+done
+
+cfg="cfg-v.png"
+bbcfg="bbcfg-v.png"
+line="line-v.txt"
+proname="cu_ran_test"
+# proname="vector_add"
 
 cuobjdump $proname -xelf ${proname}.1.sm_86.cubin
 nvdisasm -cfg ${proname}.1.sm_86.cubin | dot -o$cfg -Tpng
 nvdisasm -bbcfg ${proname}.1.sm_86.cubin | dot -o$bbcfg -Tpng
-nvdisasm -gi ${proname}.1.sm_86.cubin > $line 2>&1
+nvdisasm -gi ${proname}.1.sm_86.cubin >$line 2>&1
 
-mv $cfg $bbcfg $line $script_dir/info
+mv -f $cfg $bbcfg $line $script_dir/info
 cd $script_dir && rm -rf build
 
 # git
 cd ..
 git add .
 git commit -a -m "get info of $proname"
+git push
--- cuda/src/memory-test/memory-test.cu
+++ cuda/src/memory-test/memory-test.cu
@@ -4,18 +4,19 @@
 __global__ void read_random_arr(int *arr_gpu, const int ARR_SIZE, const int inter_cycle)
 {
     uint32_t threadid = getThreadIdInBlock();
+#pragma unroll
     for (int j = 0; j < inter_cycle; j++)
     {
-        // int i = threadid;
+        int i = threadid;
 #pragma unroll
-        // while (i < ARR_SIZE)
-        // {
-        //     i = arr_gpu[i] + 31;
-        // }
-        for (int i = threadid; i < ARR_SIZE; i += 32)
+        while (i < ARR_SIZE)
         {
-            arr_gpu[i] |= i & 1;
+            i = arr_gpu[i] + 31;
         }
+        // for (int i = threadid; i < ARR_SIZE; i += 32)
+        // {
+        //     arr_gpu[i] |= i & 1;
+        // }
     }
 }
 
