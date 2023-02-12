0b42cc0bbc2dc84ce55d97337d268b15d737d235
bric-U <chuxuange@gmail.com>
2023-02-12 15:16:30 +0800

    add filename

--- cuda/src/dissect/reference/fine_grain_L2-cold_1GB.cu
+++ cuda/src/dissect/reference/fine_grain_L2-cold_1GB.cu
@@ -1,7 +1,5 @@
-# include <stdio.h>
-# include <stdint.h>
-
-# include "cuda_runtime.h"
+#include "myutil.hpp"
+#include "util.cuh"
 
 //compile nvcc *.cu -o test
 
@@ -29,18 +27,18 @@ void measure_global() {
 	int N, iterations, stride; 
 	//stride in element
 	iterations = 1;
-	
+	char *filename = "L2cachedata.csv";
 	N = 1024 * 1024* 1024/sizeof(unsigned int); //in element
 	for (stride = 1; stride <= N/2; stride*=2) {
 		printf("\n=====%d GB array, cold cache miss, read 256 element====\n", N/1024/1024/1024);
 		printf("Stride = %d element, %d bytes\n", stride, stride * sizeof(unsigned int));
-		parametric_measure_global(N, iterations, stride );
+		parametric_measure_global(N, iterations, stride ,filename);
 		printf("===============================================\n\n");
 	}
 }
 
 
-void parametric_measure_global(int N, int iterations, int stride) {
+void parametric_measure_global(int N, int iterations, int stride, char *filename) {
 	cudaDeviceReset();
 	
 	int i;
@@ -51,17 +49,16 @@ void parametric_measure_global(int N, int iterations, int stride) {
 	/* allocate arrays on GPU */
 	cudaMalloc ((void **) &d_a, sizeof(unsigned int) * (N+2));
 
-   	/* initialize array elements on CPU with pointers into d_a. */
-	
-	for (i = 0; i < N; i++) {		
+   	/* initialize array elements on CPU with pointers into d_a. */	
+	for (i = 0; i < N; i++) {
 	//original:	
-		h_a[i] = (i+stride)%N;	
+		h_a[i] = (i+stride)%N;
 	}
 
 	h_a[N] = 0;
 	h_a[N+1] = 0;
 	/* copy array elements from CPU to GPU */
-        cudaMemcpy(d_a, h_a, sizeof(unsigned int) * N, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_a, h_a, sizeof(unsigned int) * N, cudaMemcpyHostToDevice);
 
 	unsigned int *h_index = (unsigned int *)malloc(sizeof(unsigned int)*256);
 	unsigned int *h_timeinfo = (unsigned int *)malloc(sizeof(unsigned int)*256);
@@ -72,17 +69,11 @@ void parametric_measure_global(int N, int iterations, int stride) {
 	unsigned int *d_index;
 	cudaMalloc( (void **) &d_index, sizeof(unsigned int)*256 );
 
-
-
-
 	cudaThreadSynchronize ();
 	/* launch kernel*/
 	dim3 Db = dim3(1);
 	dim3 Dg = dim3(1,1,1);
-
-
 	global_latency <<<Dg, Db>>>(d_a, N, iterations,  duration, d_index);
-
 	cudaThreadSynchronize ();
 
 	cudaError_t error_id = cudaGetLastError();
@@ -92,33 +83,57 @@ void parametric_measure_global(int N, int iterations, int stride) {
 
 	/* copy results from GPU to CPU */
 	cudaThreadSynchronize ();
+    cudaMemcpy((void *)h_timeinfo, (void *)duration, sizeof(unsigned int)*256, cudaMemcpyDeviceToHost);
+    cudaMemcpy((void *)h_index, (void *)d_index, sizeof(unsigned int)*256, cudaMemcpyDeviceToHost);
+	cudaThreadSynchronize ();\
 
+    // 如果输出文件不存在，则创建文件并写入标题
+    if (!isFileExists(filename))
+    {
+        // 读写文件。文件存在则追加写入，不存在则创建一个新文件
+        FILE *fp = fopen(filename, "a+");
+        // 如果打开文件失败
+        if (fp == NULL)
+        {
+            // std::cout << "Can't open file : " << filename << std::endl;
+            printf("Can't open file : %s \n", filename);
+            fprintf(stderr, "fopen() failed.\n");
+            exit(EXIT_FAILURE);
+        }
+        // 标题
+        fprintf(fp, "Index,Exec_time\n");
+        fclose(fp);
+    }
 
+    // 读写文件。文件存在则追加写入，不存在则创建一个新文件
+    FILE *fp = fopen(filename, "a+");
+    // 如果打开文件失败
+    if (fp == NULL)
+    {
+        // std::cout << "Can't open file : " << filename << std::endl;
+        printf("Can't open file : %s \n", filename);
+        fprintf(stderr, "fopen() failed.\n");
+        exit(EXIT_FAILURE);
+    }
 
-        cudaMemcpy((void *)h_timeinfo, (void *)duration, sizeof(unsigned int)*256, cudaMemcpyDeviceToHost);
-        cudaMemcpy((void *)h_index, (void *)d_index, sizeof(unsigned int)*256, cudaMemcpyDeviceToHost);
-
-	cudaThreadSynchronize ();
-
-	for(i=0;i<256;i++)
-		printf("%d\t %d\n", h_index[i], h_timeinfo[i]);
+	for(i=0;i<256;i++){
+        fprintf(fp, "%d,%d\n", h_index[i], h_timeinfo[i]);
+        printf("%d\t %d\n", h_index[i], h_timeinfo[i]);
+    }
+	fclose(fp);	
 
 	/* free memory on GPU */
 	cudaFree(d_a);
 	cudaFree(d_index);
 	cudaFree(duration);
 
-
-        /*free memory on CPU */
-        free(h_a);
-        free(h_index);
+    /*free memory on CPU */
+    free(h_a);
+    free(h_index);
 	free(h_timeinfo);
 	
-	cudaDeviceReset();	
-
+	cudaDeviceReset();
 }
-
-
 
 __global__ void global_latency (unsigned int * my_array, int array_length, int iterations, unsigned int * duration, unsigned int *index) {
 
@@ -135,16 +150,16 @@ __global__ void global_latency (unsigned int * my_array, int array_length, int i
 		s_tvalue[k] = 0;
 	}
 
-	for (k = 0; k < iterations*256; k++) {
-		
+    /* for loop 256 times */
+	for (k = 0; k < iterations*256; k++) {		
 		start_time = clock();
 		
 		j = my_array[j];
 		s_index[k]= j;
 		end_time = clock();
 
+        /* record execution time */
 		s_tvalue[k] = end_time-start_time;
-
 	}
 
 	my_array[array_length] = j;
