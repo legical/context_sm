6eba651982bb4f04955d3a1a6c01054b9bb91e2c
bric-U <chuxuange@gmail.com>
2023-02-15 15:24:21 +0800

    1070&3060 aoto choose

--- cuda/src/dissect_page/dissect.sh
+++ cuda/src/dissect_page/dissect.sh
@@ -4,6 +4,10 @@ script_dir=$(
     cd $(dirname $0)
     pwd
 )
+# 3060 or 1070
+GPU_name=$(nvidia-smi -q | grep "Product Name" | awk -F ' ' '{print $NF}')
+echo "You are running in GPU $GPU_name."
+
 # /home/bric/Workspace/context_sm/cuda/src/dissect_page
 proj_dir="$script_dir/../.."
 # /home/bric/Workspace/context_sm/cuda
@@ -30,25 +34,45 @@ echo -e "\033[34mStart compiling the project......\033[0m"
 cmake .. && make
 
 # 检查csv输出目录是否存在
-rm -rf $script_dir/data
-mkdir -m 754 $script_dir/data
-mkdir -m 754 $script_dir/data/log
-mkdir -m 754 $script_dir/data/pic
+rm -rf $script_dir/data-$GPU_name
+mkdir -m 754 $script_dir/data-$GPU_name
+mkdir -m 754 $script_dir/data-$GPU_name/log
+mkdir -m 754 $script_dir/data-$GPU_name/pic
 echo -e "\033[34m.csv & pic file output directory successfully created.\033[0m"
 
+# default : 3060
 inner_cycle=426
+CUDA_TOOL_DIR="/usr/local/cuda-11.7/bin"
+if [ $GPU_name -eq 1070 ]; then
+    inner_cycle=362
+    CUDA_TOOL_DIR="/usr/local/cuda-11.8/bin"
+fi
+
 for ((j = 1; j <= 5; j++)); do
-    # 3060 L2 cache: 2359296B = 2.25MB = 576 * 4KB
-    # 476 526 576 626 676
+    # 3060 L2 cache: 2359296B = 2.25MB = 576 * 4KB      476 526 576 626 676
+    # 1070 L2 cache: 2097152B = 2MB    = 512 * 4KB      412 462 512 562 612
     inner_cycle=$((inner_cycle + 50))
     echo " Starting running kernel, $inner_cycle * 1024 times."
     echo -e "Index \t Time"
     for ((i = 1; i <= 1024; i++)); do
-        # get sudo right
-        echo "0923326" | sudo -S /usr/local/cuda-11.7/bin/ncu --section MemoryWorkloadAnalysis ./l2_dissect_test $inner_cycle $i | tee -a $script_dir/data/log/dis-${inner_cycle}.log
-        sudo chmod 777 $script_dir/data/Dissect-inner${inner_cycle}.csv
-        tail -n 4 $script_dir/data/log/dis-${inner_cycle}.log | grep "L2 Hit Rate" | awk -F ' ' '{print $NF}' >>$script_dir/data/Dissect-inner${inner_cycle}.csv
-        # ./l2_dissect_test $inner_cycle $i
+        if [ $GPU_name -eq 3060 ]; then
+            # get sudo right
+            echo "0923326" | sudo -S $CUDA_TOOL_DIR/ncu --section MemoryWorkloadAnalysis ./l2_dissect_test $inner_cycle $i | tee -a $script_dir/data-$GPU_name/log/dis-${inner_cycle}.log
+            sudo chmod 777 $script_dir/data-$GPU_name/Dissect-inner${inner_cycle}.csv
+            tail -n 4 $script_dir/data/log/dis-${inner_cycle}.log | grep "L2 Hit Rate" | awk -F ' ' '{print $NF}' >>$script_dir/data-$GPU_name/Dissect-inner${inner_cycle}.csv
+        elif [ $GPU_name -eq 1070 ]; then
+            # temp log
+            /usr/bin/script -qf data-$GPU_name.log
+            echo "neu" | sudo -S /usr/local/cuda-11.8/bin/nvprof --metrics l2_tex_hit_rate ./l2_dissect_test
+            exit
+            # save info line to true log 
+            cat data-$GPU_name.log | grep "l2_tex_hit_rate" | tail -n 1 >>$script_dir/data-$GPU_name/log/dis-${inner_cycle}.log
+            echo "neu" | sudo -S chmod 777 $script_dir/data-$GPU_name/Dissect-inner${inner_cycle}.csv
+            # save hit rate info to csv file
+            cat data-$GPU_name.log | grep "l2_tex_hit_rate" | awk -F ' ' '{print $NF}' | tail -n 1 | sed 's/%//g' >>$script_dir/data-$GPU_name/Dissect-inner${inner_cycle}.csv
+        else
+            echo "Sorry, not support for $GPU_name."
+        fi
     done
 done
 rm -rf ./*
@@ -60,13 +84,13 @@ cd $script_dir
 python_model_check numpy
 python_model_check matplotlib
 
-python3 draw.py
-echo -e "\n\033[34m$script_dir/data\033[0m to see execution time data."
-echo -e "\033[34m$script_dir/data/pic\033[0m to see line charts."
+python3 draw.py $GPU_name
+echo -e "\n\033[34m$script_dir/data-$GPU_name\033[0m to see execution time data."
+echo -e "\033[34m$script_dir/data-$GPU_name/pic\033[0m to see line charts."
 echo -e "\033[32mALL done."
 
 # git
 cd $proj_dir && cd ..
 git add .
-git commit -a -m "get data of l2_dissect_test in 3060"
+git commit -a -m "get data of l2_dissect_test of $GPU_name"
 git push
--- cuda/src/dissect_page/draw.py
+++ cuda/src/dissect_page/draw.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 from cProfile import label
 import os
+import sys
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
-# example: python3 draw.py
+# example: python3 draw.py 3060
 
 
-def readname():
-    filePath = "./data/"
+def readname(filePath):
+    # default: ./data-3060/
+    # filePath = './data-'+GPU_name+'/'
     name = os.listdir(filePath)
     csv = []
     for file in name:
@@ -44,7 +46,17 @@ def get_data(filename, EXEClist, hit_rate):
                 print(row[0], 'reading data error!\n')
 
 
-csvlist = readname()
+# default : 3060
+GPU_name = "3060"
+if len(sys.argv) > 1:
+    GPU_name = str(sys.argv[1])
+
+
+# default: ./data-3060/
+DATA_DIR = './data-'+GPU_name+'/'
+PIC_DIR='./data-'+GPU_name+'/pic/'
+
+csvlist = readname(DATA_DIR)
 # 创建了一个空的7列的二维数组
 # inner time_min time_max time_avg hit_rate_min hit_rate_max hit_rate_avg
 data_analysis = np.empty(shape=[0, 7], dtype=float)
@@ -54,7 +66,7 @@ data_analysis = np.empty(shape=[0, 7], dtype=float)
 for file in csvlist:
     # 内部循环次数
     inner = get_inner(file)
-    filename = './data/'+file
+    filename = DATA_DIR+file
     # print("\n",filename)
     # 图片dpi=220，尺寸宽和高，单位为英寸
     fig = plt.figure(dpi=220, figsize=(80, 32))
@@ -64,7 +76,8 @@ for file in csvlist:
     EXEClist, hit_rate = [], []
     get_data(filename, EXEClist, hit_rate)
     # 添加当前inner的数据
-    data_analysis = np.append(data_analysis, [[inner, min(EXEClist), max(EXEClist), np.mean(EXEClist), min(hit_rate), max(hit_rate), np.mean(hit_rate)]], axis=0)
+    data_analysis = np.append(data_analysis, [[inner, min(EXEClist), max(EXEClist), np.mean(
+        EXEClist), min(hit_rate), max(hit_rate), np.mean(hit_rate)]], axis=0)
     IDlist = np.arange(1, len(EXEClist)+1)
     ax1.plot(IDlist, EXEClist, "g", marker='D',
              markersize=5, label="Execution time")
@@ -94,7 +107,7 @@ for file in csvlist:
     plt.title(chart_title.format(
         inner, min(EXEClist), max(EXEClist), np.mean(EXEClist), min(hit_rate), max(hit_rate), np.mean(hit_rate)), fontsize=46)
     # plt.gcf().autofmt_xdate()
-    filename = filename.replace("./data/", "./data/pic/", 1)
+    filename = filename.replace(DATA_DIR, PIC_DIR, 1)
     pic_name = filename.replace("csv", "jpg", 1)
     # 如果图片文件已存在，则删除
     if os.path.exists(pic_name):
@@ -148,7 +161,7 @@ plt.tick_params(labelsize=32)  # 刻度字体大小
 # 折线图标题
 plt.title('Data Analysis')
 # plt.gcf().autofmt_xdate()
-pic_name = "./data/pic/data_analysis.jpg"
+pic_name = PIC_DIR + "data_analysis.jpg"
 # 如果图片文件已存在，则删除
 if os.path.exists(pic_name):
     os.remove(pic_name)
