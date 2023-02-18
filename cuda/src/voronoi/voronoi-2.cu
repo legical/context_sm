3f1d39771794763847c461fd2155821feaef2a3c
CXG-L <chuxuange@gmail.com>
2023-02-18 23:16:45 +0800

    使用C++标准库的随机数生成器

--- cuda/src/voronoi/voronoi-2.cu
+++ cuda/src/voronoi/voronoi-2.cu
@@ -1,113 +1,101 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <cuda_runtime.h>
-#include <curand.h>
-#include <curand_kernel.h>
-#include <time.h>
-#include "jpeglib.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+#include <cmath>
+#include <jpeglib.h>
 
-#define WIDTH 1024
-#define HEIGHT 1024
-#define NUM_POINTS 1024
-#define BLOCK_SIZE 16
+// 定义常量
+const int WIDTH = 1024;
+const int HEIGHT = 1024;
+const int NUM_POINTS = 10000;
+const int BLOCK_SIZE = 16;
 
-// 定义点结构体
-typedef struct
+// 生成Voronoi图的CUDA核
+__global__ void voronoi(float2 *points, float3 *colors, unsigned char *image)
 {
-    float x, y;
-    int index;
-} Point;
+    // 获取当前线程对应的像素坐标
+    int x = threadIdx.x + blockIdx.x * blockDim.x;
+    int y = threadIdx.y + blockIdx.y * blockDim.y;
 
-// 定义颜色结构体
-typedef struct
-{
-    int r, g, b;
-} Color;
+    if (x >= WIDTH || y >= HEIGHT)
+    {
+        return;
+    }
 
-// 定义生成Voronoi图的CUDA内核
-__global__ void voronoi(Point *points, Color *colors, unsigned char *image)
-{
-    int x = blockIdx.x * blockDim.x + threadIdx.x;
-    int y = blockIdx.y * blockDim.y + threadIdx.y;
-    int minIndex = 0;
-    float minDistance = sqrtf(WIDTH * WIDTH + HEIGHT * HEIGHT);
+    // 计算当前像素到每个点的距离，并找到最近的点
+    float min_distance = INFINITY;
+    int min_index = -1;
     for (int i = 0; i < NUM_POINTS; i++)
     {
-        float dx = points[i].x - x;
-        float dy = points[i].y - y;
-        float distance = sqrtf(dx * dx + dy * dy);
-        if (distance < minDistance)
+        float2 point = points[i];
+        float distance = sqrtf((point.x - x) * (point.x - x) + (point.y - y) * (point.y - y));
+        if (distance < min_distance)
         {
-            minDistance = distance;
-            minIndex = i;
+            min_distance = distance;
+            min_index = i;
         }
     }
-    Color color = colors[minIndex];
+
+    // 将当前像素设置为最近点的颜色
+    float3 color = colors[min_index];
     int index = (y * WIDTH + x) * 3;
-    image[index] = color.r;
-    image[index + 1] = color.g;
-    image[index + 2] = color.b;
+    image[index] = (unsigned char)(color.x * 255);
+    image[index + 1] = (unsigned char)(color.y * 255);
+    image[index + 2] = (unsigned char)(color.z * 255);
 }
 
 int main()
 {
-    // 初始化CUDA设备
-    cudaDeviceProp deviceProp;
-    cudaGetDeviceProperties(&deviceProp, 0);
-    printf("Using device %s\n", deviceProp.name);
-    cudaSetDevice(0);
-
-    // 分配设备内存
-    Point *d_points;
-    Color *d_colors;
-    unsigned char *d_image;
-    cudaMalloc(&d_points, sizeof(Point) * NUM_POINTS);
-    cudaMalloc(&d_colors, sizeof(Color) * NUM_POINTS);
-    cudaMalloc(&d_image, sizeof(unsigned char) * WIDTH * HEIGHT * 3);
-
     // 初始化随机数生成器
-    curandState *d_state;
-    cudaMalloc(&d_state, sizeof(curandState) * NUM_POINTS);
-    srand(time(NULL));
-    initCurand<<<NUM_POINTS / BLOCK_SIZE, BLOCK_SIZE>>>(d_state, time(NULL));
+    std::default_random_engine generator(time(nullptr));
+    std::uniform_real_distribution<float> distribution(0.0f, 1.0f);
 
-    // 初始化点和颜色
-    Point *points = (Point *)malloc(sizeof(Point) * NUM_POINTS);
-    Color *colors = (Color *)malloc(sizeof(Color) * NUM_POINTS);
+    // 分配内存并初始化点和颜色数组
+    float2 *points = (float2 *)malloc(sizeof(float2) * NUM_POINTS);
+    float3 *colors = (float3 *)malloc(sizeof(float3) * NUM_POINTS);
     for (int i = 0; i < NUM_POINTS; i++)
     {
-        points[i].x = curand_uniform(&d_state[i]) * WIDTH;
-        points[i].y = curand_uniform(&d_state[i]) * HEIGHT;
-        points[i].index = i;
-        colors[i].r = rand() % 256;
-        colors[i].g = rand() % 256;
-        colors[i].b = rand() % 256;
+        points[i].x = distribution(generator) * WIDTH;
+        points[i].y = distribution(generator) * HEIGHT;
+        colors[i].x = distribution(generator);
+        colors[i].y = distribution(generator);
+        colors[i].z = distribution(generator);
     }
 
-    // 将点和颜色拷贝到设备
-    cudaMemcpy(d_points, points, sizeof(Point) * NUM_POINTS, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_colors, colors, sizeof(Color) * NUM_POINTS, cudaMemcpyHostToDevice);
+    // 在GPU上分配内存
+    float2 *d_points;
+    float3 *d_colors;
+    unsigned char *d_image;
+    cudaMalloc((void **)&d_points, sizeof(float2) * NUM_POINTS);
+    cudaMalloc((void **)&d_colors, sizeof(float3) * NUM_POINTS);
+    cudaMalloc((void **)&d_image, sizeof(unsigned char) * WIDTH * HEIGHT * 3);
 
-    // 调用CUDA内核生成Voronoi图
-    dim3 blocks(WIDTH / BLOCK_SIZE, HEIGHT / BLOCK_SIZE);
-    dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
-    voronoi<<<blocks, threads>>>(d_points, d_colors, d_image);
-    cudaDeviceSynchronize();
+    // 将数据拷贝到GPU
+    cudaMemcpy(d_points, points, sizeof(float2) * NUM_POINTS, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_colors, colors, sizeof(float3) * NUM_POINTS, cudaMemcpyHostToDevice);
+
+    // 设置CUDA内核的网格和块大小
+    dim3 gridSize((WIDTH + BLOCK_SIZE - 1) / BLOCK_SIZE, (HEIGHT + BLOCK_SIZE - 1) / BLOCK_SIZE);
+    dim3 blockSize(BLOCK_SIZE, BLOCK_SIZE);
+
+    // 调用Voronoi图生成核函数
+    voronoi<<<gridSize, blockSize>>>(d_points, d_colors, d_image);
+
     // 将结果拷贝回主机
     unsigned char *image = (unsigned char *)malloc(sizeof(unsigned char) * WIDTH * HEIGHT * 3);
     cudaMemcpy(image, d_image, sizeof(unsigned char) * WIDTH * HEIGHT * 3, cudaMemcpyDeviceToHost);
 
-    // 保存Voronoi图为JPG文件
+    // 保存结果为JPEG文件
     struct jpeg_compress_struct cinfo;
     struct jpeg_error_mgr jerr;
-    FILE *outfile;
-    JSAMPROW row_pointer[1];
     cinfo.err = jpeg_std_error(&jerr);
     jpeg_create_compress(&cinfo);
-    if ((outfile = fopen("../src/voronoi/voronoi2.jpg", "wb")) == NULL)
+    FILE *outfile;
+    if ((outfile = fopen("voronoi.jpg", "wb")) == NULL)
     {
-        fprintf(stderr, "Can't open output file\n");
-        exit(1);
+        std::cout << "Error opening output JPEG file!" << std::endl;
+        return 1;
     }
     jpeg_stdio_dest(&cinfo, outfile);
     cinfo.image_width = WIDTH;
@@ -116,13 +104,15 @@ int main()
     cinfo.in_color_space = JCS_RGB;
     jpeg_set_defaults(&cinfo);
     jpeg_start_compress(&cinfo, TRUE);
+    JSAMPROW row_pointer[1];
     while (cinfo.next_scanline < cinfo.image_height)
     {
-        row_pointer[0] = &image[cinfo.next_scanline * cinfo.image_width * 3];
+        row_pointer[0] = &image[cinfo.next_scanline * WIDTH * 3];
         jpeg_write_scanlines(&cinfo, row_pointer, 1);
     }
     jpeg_finish_compress(&cinfo);
     fclose(outfile);
+    jpeg_destroy_compress(&cinfo);
 
     // 释放内存
     free(points);
@@ -131,7 +121,6 @@ int main()
     cudaFree(d_points);
     cudaFree(d_colors);
     cudaFree(d_image);
-    cudaFree(d_state);
 
     return 0;
 }
